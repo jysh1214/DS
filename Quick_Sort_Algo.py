3854f4c88ff0574e765fe380dbe88a727156bcd5
jysh1214 <jyxemperor@gmail.com>
2018-01-09 23:03:53 +0800

    Create Quick_Sort_Algo.py

---
+++ Quick_Sort_Algo.py
@@ -0,0 +1,34 @@
+import random
+
+def Quicksort(A, p, r):
+    if (p<r):
+        [A, q] = Partition(A, p, r)
+        #A[p:q] = 
+        Quicksort(A, p, q)
+        #A[q+1:r] = 
+        Quicksort(A, q+1, r)
+
+def Partition(A, p, r):
+    x = A[r-1]
+    i = p - 1
+    for j in range(p, r-1):
+        if (A[j]<=x):
+            i += 1
+            A[i], A[j] = A[j], A[i]
+
+    A[r-1], A[i+1] = A[i+1], A[r-1]
+    return A,i+1
+
+def main():
+    global A # show clear
+    length = 10
+    A = [random.randint(0, 100) for i in range(length)]
+    A = [x for x in A]
+
+    n = len(A)
+    print(A)
+    Quicksort(A, 0, n)
+    print(A)
+
+if __name__ == '__main__':
+    main()
