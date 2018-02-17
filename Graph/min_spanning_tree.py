067eaa9b84a68963f6514e397d88758c739ada69
jysh1214 <jyxemperor@gmail.com>
2018-02-17 20:13:39 +0800

    Update min_spanning_tree.py

--- Graph/min_spanning_tree.py
+++ Graph/min_spanning_tree.py
@@ -1,4 +1,5 @@
 from get_imformation import GI
+from disjoint_set import DS
 
 class MST():
     def __init__(self, adj_matrix, ins_matrix):
@@ -7,20 +8,25 @@ class MST():
         self.Insidence_Matrix = ins_matrix
 
     def kruskal_algo(self):
-        E = [i for i in range(len(self.Insidence_Matrix[0]))]
-
         # from get_imformation
         gi = GI(self.Adjacency_Matrix, self.Insidence_Matrix)
+        E = [(i, gi.get_weight(i)) for i in range(len(self.Insidence_Matrix[0]))]
+
         # edges sorted by weight
-        E = sorted(E, key = lambda x: gi.get_wieght(x))
+        E = sorted(E, key = lambda x: x[1])
+        for i in range(len(E)):
+            E[i] = E[i][0]
+
         con_ver = []
         mst = []
 
-        i = 0
-        while len(con_ver) < self.N:
-            (v_a, v_b) = gi.get_term(E[i])
-            if (v_a in con_ver) and (v_b in con_ver):
-            	pass # the edge is choosen or could make circle
+        for i in range(len(E)):
+            (v_a, v_b) = gi.edge_term(E[i])
+            
+            # from disjoint_set
+            ds = DS(self.Insidence_Matrix, con_ver, mst, v_a, v_b)
+            if ds.same_set(v_a, v_b):
+                pass # the edge is choosen or could make circle
             else:
                 mst.append(E[i])
                 if not (v_a in con_ver):
@@ -29,7 +35,7 @@ class MST():
                     con_ver.append(v_b)
 
         return mst
-
+ 
 def put_all(a, b):
     for i in a:
         b.append(i)
