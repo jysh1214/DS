c4aaccf495a8cde831e89deab8cce610cf8e490e
jysh1214 <jyxemperor@gmail.com>
2018-02-22 01:14:11 +0800

    Update shortest_path_problem.py

--- Graph/shortest_path_problem.py
+++ Graph/shortest_path_problem.py
@@ -1,6 +1,7 @@
 from get_imformation import GI
 
 import math
+import numpy as np
 
 class SP():
     def __init__(self, adj_matrix, ins_matrix):
@@ -16,13 +17,13 @@ class SP():
         Raises:
             ValueError, TypeError
         """
-        s = []
+        sv = [] # selected vertices
         dist = [math.inf for i in range(self.N)]
         dist[start] = 0
 
         select = start
-        while len(s) < self.N - 1:
-            s.append(select)
+        while len(sv) < self.N - 1:
+            sv.append(select)
             # from get_imformation
             gi = GI(self.Adjacency_Matrix, self.Insidence_Matrix)
             nb = gi.get_nb(select)
@@ -40,13 +41,13 @@ class SP():
             temp = sorted(temp, key = lambda x: x[1])
 
             for k in range(len(temp)):
-                if not(k in s):
+                if not(k in sv):
                     select = k
                     break
 
         # last one vertex
         total = [i for i in range(self.N)]
-        last = list(set(total)-set(s))[0]
+        last = list(set(total)-set(sv))[0]
         # from get_imformation
         gi = GI(self.Adjacency_Matrix, self.Insidence_Matrix)
         nb = gi.get_nb(last)
@@ -57,3 +58,59 @@ class SP():
                 dist[last] = w + dist[nb[i]]
 
         return dist
+
+    def bellman_ford_algo(self, start):
+        """
+        Returns:
+            dist(list): Start vertex to all the others vertices distance.
+
+        Raises:
+            ValueError, TypeError
+        """
+        dist = [[math.inf for i in range(self.N)]]
+        dist[0][start] = 0
+
+        # bool. matrix show fixed distance vertex
+        bool_matrix = np.matrix(self.Adjacency_Matrix)
+        # matrix[x, y] = list[x][y]
+        for i in range(self.N):
+            for j in range(self.N):
+                if self.Adjacency_Matrix[i][j] != 0:
+                    bool_matrix[i, j] = 1
+                else:
+                    bool_matrix[i, j] = 0
+
+        step = 1
+        while step < self.N - 1:
+            dist.append([math.inf for i in range(self.N)])
+            dist[step][start] = 0
+            arrival_list = []
+            # collect vertices can arrice
+            for k in range(len(bool_matrix)):
+                if bool_matrix[start, k]:
+                    arrival_list.append(k)
+
+            for l in range(len(arrival_list)):
+                n = arrival_list[l]
+                # collect u: vertex can go to n
+                u = []
+                for t in range(self.N):
+                    if self.Adjacency_Matrix[t][n] != 0:
+                        u.append(t)
+
+                # find min(dist[step-1][u]+weight(u, n))
+                min_ = math.inf
+                for r in range(len(u)):
+                    # from get_information
+                    gi = GI(self.Adjacency_Matrix, self.Insidence_Matrix)
+                    e = gi.get_edge(u[r], n)
+                    if dist[step-1][u[r]] + gi.get_weight(e) < min_:
+                        min_ = dist[step-1][u[r]] + gi.get_weight(e)
+
+                dist[step][n] = min(dist[step-1][n], min_)
+
+            step += 1
+            bool_matrix *= bool_matrix
+            # while end
+
+        return dist[step-1]
