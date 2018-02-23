41859aeb403915be82876e3c1cdaab521b1e25eb
jysh1214 <jyxemperor@gmail.com>
2018-02-24 00:08:58 +0800

    Update get_imformation.py

--- Graph/get_imformation.py
+++ Graph/get_imformation.py
@@ -3,6 +3,60 @@ class GI():
         self.Adjacency_Matrix = adj_matrix
         self.Insidence_Matrix = ins_matrix
 
+    def get_degree(self, vertex):
+        """
+        Parameters:
+            vertex(int): Vertex No..
+
+            vertex(str): Vertex name.
+
+        Returns:
+            self.degree[vertex](int): Degree of the input vertex.
+
+        Attention:
+            Undirected graph difinition.
+
+        Raises:
+            ValueError, TypeError
+        """
+        try:
+            int(vertex)
+        except:
+            vertex = name_to_num(vertex, self.V)
+
+        degree = 0
+        for i in range(self.N):
+            if self.Adjacency_Matrix[vertex][i] != 1:
+                degree += 1
+
+        return degree
+
+    def get_in_degree(self, vertex):
+        try:
+            int(vertex)
+        except:
+            vertex = name_to_num(vertex, self.V)
+
+        in_degree = 0
+        for i in range(self.N):
+            if self.Adjacency_Matrix[i][vertex] != 0:
+                in_degree += 1
+
+        return in_degre
+
+    def get_out_degree(self, vertex):
+        try:
+            int(vertex)
+        except:
+            vertex = name_to_num(vertex, self.V)
+
+        out_degree = 0
+        for i in range(self.N):
+            if self.Adjacency_Matrix[vertex][i] != 0:
+                out_degree += 1
+
+        return out_degree
+
     def get_nb(self, vertex):
         """
         Parameters:
@@ -67,6 +121,6 @@ class GI():
         temp = []
         for i in range(len(self.Insidence_Matrix)):
             if self.Insidence_Matrix[i][edge] == 1:
-                temp.append(self.Insidence_Matrix[i][edge])
-            if len(temp) == 2: break
-        return temp
+                temp.append(i)
+            if len(temp) == 2:
+                return temp
